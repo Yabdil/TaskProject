30d6c7e48b9dfe4c4e398ea87a3a3db06e4c06a6
Abdillahi Abdi <61968530+Yabdil@users.noreply.github.com>
2022-05-16 14:07:05 +0200

    Added test for Task

--- TaskApp/tests.py
+++ TaskApp/tests.py
@@ -1,13 +1,26 @@
+import os
+
+from dotenv import load_dotenv
 from django.test import TestCase
 from .models import CustomUser, Task
+
+load_dotenv()
+
+EMAIL = os.getenv("EMAIL")
+PASSWORD = os.getenv("password")
+FIRSTNAME = os.getenv("FIRSTNAME")
+LASTNAME = os.getenv("LASTNAME")
 
 
 class TestUser(TestCase):
     def setUp(self):
-        CustomUser.objects.create(email="test1@gmail.com", password="mypassword", first_name="User", last_name="Test")
+        CustomUser.objects.create(email=EMAIL,
+                                  password=PASSWORD,
+                                  first_name=FIRSTNAME,
+                                  last_name=LASTNAME)
 
     def test_user_added(self):
-        user_test = CustomUser.objects.get(email="test1@gmail.com")
+        user_test = CustomUser.objects.get(email=EMAIL)
 
         # Testing that the user is added in BDD with all of his information
         self.assertEqual(user_test.email, "test1@gmail.com")
@@ -15,3 +28,22 @@ class TestUser(TestCase):
         self.assertEqual(user_test.last_name, "Test")
         self.assertFalse(user_test.is_admin)
 
+
+class TestTask(TestCase):
+    def setUp(self):
+        # Adding a fake user
+        self.user = CustomUser.objects.create(email=EMAIL,
+                                              password=PASSWORD,
+                                              first_name=FIRSTNAME,
+                                              last_name=LASTNAME)
+
+        # adding a task
+        self.task = Task.objects.create(description="Premier test", created_by=self.user)
+
+    def test_task_created(self):
+        self.assertEqual(self.task.description, "Premier test")
+        self.assertEqual(self.task.created_by, self.user)
+
+    def test_task_is_not_completed(self):
+        # By default, every new added task is not completed
+        self.assertFalse(self.task.is_completed)
