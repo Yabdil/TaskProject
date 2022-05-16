22d995d9de0608173a78666956564a97f98cd34a
Abdillahi Abdi <61968530+Yabdil@users.noreply.github.com>
2022-05-16 17:08:02 +0200

    Creating html, vue and css file for the project

---
+++ TaskApp/static/vue/login.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+    el: '#mainBody',
+    delimiters: ["[[", "]]"],
+    data: function () {
+        return {
+            message: 'Haha',
+        }
+    },
+    methods: {
+
+    }
+})
+
+console.log("haha vue is charged")
